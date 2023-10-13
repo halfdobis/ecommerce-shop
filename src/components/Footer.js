42ff4cd54a558643816311249d36ed9ae5ff9c10
missdobis <missdobis@gmail.com>
2023-10-13 06:13:28 +0100

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright&copy; Ecommerce Shop {new Date().getFullYear()}. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
